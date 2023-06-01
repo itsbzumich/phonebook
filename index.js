c71ef3423f78b936c437e84d0c2e4b33530d8fde
itsbzumich <114364907+itsbzumich@users.noreply.github.com>
2023-05-31 19:40:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ const errorHandler = (error, request, response, next) => {
 app.use(errorHandler)
 
 
-  const PORT = process.env.PORT
+  const PORT = 3001
   app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
